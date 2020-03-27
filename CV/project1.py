1c09def87404a27a6fa873743544322c405a7184
urisabate <36154193+urisabate@users.noreply.github.com>
2020-03-27 19:52:21 +0100

    trying more shit

--- CV/project1.py
+++ CV/project1.py
@@ -8,20 +8,28 @@ def test1():
     iW, iH = inputImg.shape
     tw, th = target.shape
 
+    #inputImg = np.float64(inputImg)
+    #target = np.float64(target)
+
+    #cv2.normalize(inputImg, inputImg)
+    #cv2.normalize(target, target)
+
     cv2.imshow("input Image", inputImg)
     cv2.imshow("target Image", target)
-    
-    matchMap = np.float64(np.zeros((iW - tw + 1, iH - th + 1)))
-    mw, mh = matchMap.shape
+
+    matchMap = np.float64(np.zeros(shape=(iW - tw + 1, iH - th + 1)))
+    mw = iW - tw + 1
+    mh = iH - th + 1
 
     i, j = 0, 0
 
-    for i in range(0, mw):
-        for j in range(0, mh):
-            aux = np.zeros((tw - 1, th - 1))
-            aux = np.square(target[:, :] - inputImg[i:i+mw, j:j+mh])
-            matchMap[i:j] = np.sum(aux)
+    for i in range(0, mw - 1):
+        for j in range(0, mh - 1):
+           # aux = np.copy(target)
+           # aux = np.square(target[:, :] - inputImg[i:i+tw, j:j+th])
+            matchMap[i:j] = np.sum(np.square(target[:, :] - inputImg[i:i+tw, j:j+th]))
 
+   # matchMap = cv2.normalize(matchMap, matchMap)
     cv2.imshow("MatchingMap", matchMap)
 
 test1()
