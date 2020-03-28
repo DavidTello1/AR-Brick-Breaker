4bb705c8e1cf4b066f2b12873a9ce11506733da3
urisabate <36154193+urisabate@users.noreply.github.com>
2020-03-28 15:43:16 +0100

    Update project1.py

--- CV/project1.py
+++ CV/project1.py
@@ -3,30 +3,34 @@ import cv2
 
 def test1():
     inputImg = cv2.imread("img2.png", 1)
-   # inputImgGray = cv2.cvtColor(inputImg, cv2.COLOR_BGR2GRAY)
-   # iW, iH = inputImgGray.shape
+    inputImgGray = cv2.cvtColor(inputImg, cv2.COLOR_BGR2GRAY)
+    iW, iH = inputImgGray.shape
 
     target = cv2.imread("t1-img2.png", 1)
-    #targetGray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
-    #tw, th = targetGray.shape
+    targetGray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+    tw, th = targetGray.shape
 
-    # cv2.normalize(inputImg, inputImg, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    # cv2.normalize(target, target, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    # cv2.normalize(inputImgGray, inputImgGray, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    # cv2.normalize(targetGray, targetGray, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    # mw, mh = iW - tw + 1, iH - th + 1
-    # matchMap = np.zeros(shape=(mw, mh), dtype=float)
-    matchMap = cv2.matchTemplate(inputImg, target, 1)
+    mw, mh = iW - tw + 1, iH - th + 1
+    matchMap = np.zeros(shape=(mw, mh), dtype=float)
+   # matchMap = cv2.matchTemplate(inputImg, target, 1)
 
-    # for i in range(0, mw):
-    #     for j in range(0, mh):
-    #         aux = np.zeros(shape=(tw, th), dtype=float)
-    #         aux = np.square(targetGray[:, :] - inputImgGray[i:i+tw, j:j+th])
-    #         matchMap[i:j] = np.sum(aux, None, dtype=float)
+    for i in range(0, mw-1):
+        for j in range(0, mh-1):
+            aux = np.zeros(shape=(tw, th), dtype=float)
+            aux += np.square(targetGray[:, :] - inputImgGray[i:i+tw, j:j+tw])
+            matchMap[i:j] = np.sum(aux, None, dtype=float)
 
+    matchMap = matchMap / matchMap.max()
     #cv2.normalize(matchMap, matchMap)
+    #cv2.normalize(matchMap, matchMap, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
     cv2.imshow("input Image", inputImg)
     cv2.imshow("target Image", target)
+    # cv2.imshow("gray Image", inputImgGray)
+    # cv2.imshow("Gray target", targetGray)
     cv2.imshow("MatchingMap", matchMap)
 
 test1()
