e0345a81c520bd8792ebbc7080ae50a67841b1fa
urisabate <36154193+urisabate@users.noreply.github.com>
2020-03-29 14:35:47 +0200

    Update test_david.py

--- CV/test_david.py
+++ CV/test_david.py
@@ -10,53 +10,41 @@ def TemplateMathing(Input, Target, Threshold):
     # Target image GrayScale
     target_gray = cv2.cvtColor(Target, cv2.COLOR_BGR2GRAY)
     w, h = target_gray.shape
-
-    #Downscaling images
-
-    downscale = 60
-    iw = int(W * downscale / 100)
-    ih = int(H * downscale / 100)
-    rescale_input = (iw, ih)
-    resized_Igray = np.zeros((iw, ih))
-    cv2.resize(input_gray, rescale_input, resized_Igray, interpolation=cv2.INTER_AREA)
-
-    tw = int(w * downscale / 100)
-    th = int(h * downscale / 100)
-    rescale_target = (tw, th)
-    resized_Tgray = np.zeros((tw, th))
-    cv2.resize(target_gray, rescale_target, resized_Tgray, interpolation=cv2.INTER_AREA)
-
-    #---------------------------------------------------------------------------#
-#     temp_found = None
-#     for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-#         #resize the image and store the ratio
-#         resized_img = imutils.resize(gray_image, width = int(gray_image.shape[1] * scale))
-#         ratio = gray_image.shape[1] / float(resized_img.shape[1])
-#         if resized_img.shape[0] < height or resized_img.shape[1] < width:
-#             break
-#         #Convert to edged image for checking
-#         #e = cv2.Canny(resized_img, 10, 25)
-#         match = cv2.matchTemplate(e, template, cv2.TM_CCOEFF)
-#     (_, val_max, _, loc_max) = cv2.minMaxLoc(match)
-#    if temp_found is None or val_max>temp_found[0]:
-#       temp_found = (val_max, loc_max, ratio)
-# #Get information from temp_found to compute x,y coordinate
-#     (_, loc_max, r) = temp_found
-#     (x_start, y_start) = (int(loc_max[0]), int(loc_max[1]))
-#     (x_end, y_end) = (int((loc_max[0] + width)), int((loc_max[1] + height)))
-# #Draw rectangle around the template
-#     cv2.rectangle(main_image, (x_start, y_start), (x_end, y_end), (153, 22, 0), 5)
-
+#------------------------------------------------------------------------
+    # scale_percent = 50.0/ 100.0
     
-    #---------------------------------------------------------------------------#
+    # # Resize Input (60%)
+    # iH, iW = Input.shape[:2]
+    # W = int(iW*scale_percent)
+    # H = int(iH*scale_percent)
+    # resized_input = cv2.resize(Input, (W, H), interpolation=cv2.INTER_AREA)
 
+    # # Resize Target (60%)
+    # th, tw = Target.shape[:2]
+    # w = int(tw*scale_percent)
+    # h = int(th*scale_percent)
+
+    # resized_target = cv2.resize(Target, (w, h), interpolation=cv2.INTER_AREA)
+
+    # # Input image GrayScale
+    # input_gray = cv2.cvtColor(resized_input, cv2.COLOR_BGR2GRAY)
+    # target_gray = cv2.cvtColor(resized_target, cv2.COLOR_BGR2GRAY)
+
+    # print(scale_percent)
+    
+    # print(W, H)
+    # print(iW, iH)
+
+    # print(w, h)
+    # print(tw, th)
+#-------------------------------------------------------------------------
     # Create empty Matching Map
     matchMap = np.zeros(shape=(W-w+1, H-h+1), dtype=float)
 
     for x in range(0, W-w+1):
         for y in range(0, H-h+1):
             # sum of squared diferences
-            matchMap[x][y] = np.sum(np.square(resized_Tgray[:, :] - resized_Igray[x:x+tw, y:y+th]))
+            matchMap[x, y] = np.sum(np.square(target_gray[:, :] - input_gray[x:x+w, y:y+h]))
 
     matchMap = matchMap / matchMap.max()
     loc = np.where(matchMap < Threshold)
@@ -64,7 +52,7 @@ def TemplateMathing(Input, Target, Threshold):
     if loc:
         for pt in zip(*loc[::-1]):
             Found = int(True)
-            cv2.rectangle(Input, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 1)
+            cv2.rectangle(Input, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
 
     return matchMap, Found
 
